34215125d51468501ce994ada856da41ace7b7fc
shantannuu <shantanubolate@gmail.com>
2023-09-16 23:16:53 +0530

    shantanu

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
-hello Ankita 
-shut Up 
+const express = require('express');
+const app = express();
+app.use(express.json())
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+
+
+const port = process.env.PORT || 5000;
+
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "..", "/client/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"))
+    })
+}
+
+
+app.listen(port, () => console.log(`Server is running on ${port}`));
+
