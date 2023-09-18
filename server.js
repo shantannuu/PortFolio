22da00457aa5e667f22b19bfeb3855506fe212bc
Shantannuu <shantanubolate@gmail.com>
2023-09-18 22:19:30 +0530

    Ankita

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
 
 const userRoute = require("./Routes/aboutRoutes")
+const userAdminRoute = require("./routes/usersadminRoute")
 const contactRoute =require("./Routes/contactRoutes")
 const projectRoute =require("./Routes/projectRoutes")
 const contactgetRoute=require("./Routes/adminRoute")
 
 app.use("/api/users", userRoute);
+app.use("/api/adminLogin",adminRoute);
 app.use("/api/contact",contactRoute);
 app.use("/api/project",projectRoute);
 app.use("/api/admin",contactgetRoute);
